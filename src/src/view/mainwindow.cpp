bf5bdb4145aead43676d7e465cdfc9975a96eecc
Yonn Argelia <yonnarge@student.21-school.ru>
2023-08-20 01:35:17 +0300

    Add some buttons

--- src/src/view/mainwindow.cpp
+++ src/src/view/mainwindow.cpp
@@ -30,6 +30,38 @@ void s21::CalculatorWindow::typeChars() {
 
 void s21::CalculatorWindow::connectSlots() {
     connect(ui_->button_0, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_1, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_2, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_3, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_4, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_5, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_6, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_7, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_8, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_9, SIGNAL(clicked()), this, SLOT(typeChars()));
+
+    connect(ui_->button_open_bracket, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_close_bracket, SIGNAL(clicked()), this, SLOT(typeChars()));
+
+    connect(ui_->button_sum, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_sub, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_mul, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_div, SIGNAL(clicked()), this, SLOT(typeChars()));
+
+    connect(ui_->button_dot, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_degree, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_x, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_sin, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_cos, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_tan, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_asin, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_acos, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_atan, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_log, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_ln, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_mod, SIGNAL(clicked()), this, SLOT(typeChars()));
+    connect(ui_->button_sqrt, SIGNAL(clicked()), this, SLOT(typeChars()));
+
 
 }
 
